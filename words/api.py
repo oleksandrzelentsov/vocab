3d733eef2dafe692b4074aafc53e3e0188a916bf
Oleksandr Zelentsov <saze1997@gmail.com>
2017-05-25 23:50:39 +0200

    formatting, IDEA warnings fix

--- words/api.py
+++ words/api.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from vocab.settings import DEFAULT_LANGUAGE, WORD_FILES
 from django.core.cache import cache
+from django.http import JsonResponse
+
 from .models import update_word_cache
-
-
-# words = {'a', 'o', 'i', 'e', 'u'}
 
 
 def api(request):
@@ -27,18 +23,19 @@ def api(request):
             info['score'] += 1
             info['words'].append(word)
             cache.set(guid, info, 60 * 60)
-            return JsonResponse({'result': 'success',
-                                 'func': 'print' if func == 'debug' else 'display_success',
-                                 'what': word + ' ' + guid + ' ' + ', '.join(cache.get(guid)['words']),
-                                 'score': info['score'],
-                                 'words': cache.get(guid)['words'],
-                                })
+            return JsonResponse({
+                'result': 'success',
+                'func': 'print' if func == 'debug' else 'display_success',
+                'what': word + ' ' + guid + ' ' + ', '.join(cache.get(guid)['words']),
+                'score': info['score'],
+                'words': cache.get(guid)['words'],
+            })
 
         cache.set(guid, info, 60 * 60)
-        return JsonResponse({'result': 'success',
-                             'func': 'check_failed',
-                             'what': word,
-                            })
+        return JsonResponse({
+            'result': 'success',
+            'func': 'check_failed',
+            'what': word,
+        })
     except Exception as e:
         return JsonResponse({'result': str(e)})
-
--- words/models.py
+++ words/models.py
@@ -1,6 +1,7 @@
-from django.db import models
-from vocab.settings import DEFAULT_LANGUAGE, WORD_FILES
 from django.core.cache import cache
+
+from vocab.settings import DEFAULT_LANGUAGE, WORD_FILES
+
 
 # Create your models here.
 
@@ -10,4 +11,3 @@ def update_word_cache():
         with open(f, 'r') as fp:
             words += list(map(lambda x: x.strip(), fp.readlines()))
     cache.set('words', words, None)
-
